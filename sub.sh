3916c10bc429f0c19ab5d7e858f03c346e3d8f5a
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-06-05 16:02:23 +0800

    Update sub.sh
    新版超算提交脚本

--- sub.sh
+++ sub.sh
@@ -3,9 +3,10 @@
 #SBATCH -N 1
 #SBATCH -n 64
 #export OMP_NUM_THREADS=1
-source /public1/soft/modules/module.sh
-module load cp2k/8.1
+source /public21/soft/modules/module.sh
 module load gcc/7.3.0-kd
-export PATH=/public1/soft/cp2k/8.1/exe/local:$PATH
-source /public1/soft/cp2k/8.1/tools/toolchain/install/setup
-/public1/soft/cp2k/8.1/tools/toolchain/install/openmpi-4.0.5/bin/mpirun cp2k.psmp -i OT.inp -o tem.out
+module load  mpi/openmpi/4.1.1-gcc7.3.0
+export PATH=/public21/soft/cp2k/8.1/exe/local:$PATH
+source /public21/soft/cp2k/8.1/tools/toolchain/install/setup
+export CP2K_DATA_DIR=/public21/soft/cp2k/8.1/data
+mpirun cp2k.psmp -i OT.inp -o tem.out
